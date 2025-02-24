f18a4b699ce3d545de27c1afca3d5a4529d8173e
mralikeshtkar <alikeshtkar262@gmail.com>
2025-02-24 14:39:17 +0100

    change client auth key

--- src/configs/msalConfig.js
+++ src/configs/msalConfig.js
@@ -2,7 +2,7 @@ import { PublicClientApplication } from "@azure/msal-browser";
 
 const msalConfig = {
   auth: {
-    clientId: "a7009f2b-e0fc-41a7-8fa2-4841ad7af1e2",
+    clientId: "8023f59d-baeb-4507-9a07-e3efc4a54a1a",
     authority: "https://login.microsoftonline.com/common/oauth2/v2.0/authorize",
     redirectUri: "http://localhost:5173/",
   },
