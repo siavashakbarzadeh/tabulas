fdba6e5e12123ecfeda20156a34798b9de17147d
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2025-03-10 08:05:06 +0100

    wer

--- src/layout/sidebar2/Menu.jsx
+++ src/layout/sidebar2/Menu.jsx
@@ -14,84 +14,27 @@ const Menu = [
     title: "Assemblea",
     icon: <PaperIcon className="w-6 h-6" />,
     subMenu: [
-      {
-        id: 1,
-        title: "Confirm",
-        link: "/confirm",
-      },
-      {
-        id: 2,
-        title: "Email",
-        link: "/email",
-      },
-      {
-        id: 3,
-        title: "Form",
-        link: "/form",
-      },
-      {
-        id: 4,
-        title: "Services",
-        link: "/services",
-      },
-      {
-        id: 5,
-        title: "Guidemanuali",
-        link: "/guidemanuali",
-      },
-      {
-        id: 6,
-        title: "Ebook",
-        link: "/ebook",
-      },
+      { id: 1, title: "Confirm", link: "/confirm" },
+      { id: 2, title: "Email", link: "/email" },
+      { id: 3, title: "Form", link: "/form" },
+      { id: 4, title: "Services", link: "/services" },
+      { id: 5, title: "Guidemanuali", link: "/guidemanuali" },
+      { id: 6, title: "Ebook", link: "/ebook" },
       {
         id: 7,
         title: "Commissioni",
-        icon: <User className="w-6 h-6" />,
+        icon: <UsersIcon className="w-6 h-6" />,
         link: "/commissioni",
       },
-      {
-        id: 8,
-        title: "Ultimiatti",
-        link: "/ultimiatti",
-      },
-      {
-        id: 8,
-        title: "Ultimdossier",
-        link: "/ultimdossier",
-      },
+      { id: 8, title: "Ultimiatti", link: "/ultimiatti" },
+      { id: 9, title: "Ultimdossier", link: "/ultimdossier" },
     ],
   },
-  {
-    id: 2,
-    title: "Commissioni permanenti",
-    icon: <UsersIcon className="w-6 h-6" />,
-    link: "/commissioni-permanenti",
-  },
-  {
-    id: 3,
-    title: "Giunte e altre comissioni",
-    icon: <ChatIcon className="w-6 h-6" />,
-    link: "/giunte-e-altre-comissioni",
-  },
-  {
-    id: 4,
-    title: "Bicamerali e delegazioni",
-    icon: <UsersIcon className="w-6 h-6" />,
-    link: "/giunte-e-altre-comissioni",
-  },
-  {
-    id: 5,
-    title: "Diretta Senato",
-    icon: <Play2Icon className="w-6 h-6" />,
-    link: "/",
-  },
-  {
-    id: 6,
-    title: "INFORMAZIONE",
-    icon: <NoticeIcon className="w-6 h-6" />,
-    link: "/",
-  },
+  { id: 2, title: "Commissioni permanenti", icon: <User className="w-6 h-6" />, link: "/commissioni-permanenti" },
+  { id: 3, title: "Giunte e altre comissioni", icon: <ChatIcon className="w-6 h-6" />, link: "/giunte-e-altre-comissioni" },
+  { id: 4, title: "Bicamerali e delegazioni", icon: <UsersIcon className="w-6 h-6" />, link: "/giunte-e-altre-comissioni" },
+  { id: 5, title: "Diretta Senato", icon: <Play2Icon className="w-6 h-6" />, link: "/" },
+  { id: 6, title: "INFORMAZIONE", icon: <NoticeIcon className="w-6 h-6" />, link: "/" },
 ];
 
 function Menu2() {
@@ -102,7 +45,6 @@ function Menu2() {
   };
 
   return (
-    /*  opacity-70 inactive class */
     <ul className="w-full mt-4">
       {Menu.map((item) => (
         <li key={item.id} className="w-full">
@@ -116,10 +58,7 @@ function Menu2() {
               <ArraowDownIcon className={`w-2.5 transition-transform duration-150 ${active === item.id ? "" : "-rotate-90"}`} />
             </div>
           ) : (
-            <Link
-              to={item.link}
-              className="w-full h-10 flex items-center space-x-2 px-2 text-sm text-white cursor-pointer"
-            >
+            <Link to={item.link} className="w-full h-10 flex items-center space-x-2 px-2 text-sm text-white cursor-pointer">
               {item.icon}
               <span>{item.title}</span>
             </Link>
@@ -128,11 +67,9 @@ function Menu2() {
             <ul className={`w-full ${active === item.id ? "" : "hidden"} space-y-1 rounded-xl overflow-hidden`}>
               {item.subMenu.map((subItem) => (
                 <li key={subItem.id} className="w-full">
-                  <Link
-                    to={subItem.link}
-                    className="w-full h-10 flex items-center px-2 text-white bg-white/5"
-                  >
-                    {subItem.title}
+                  <Link to={subItem.link} className="w-full h-10 flex items-center px-2 text-white bg-white/5 space-x-2">
+                    {subItem.icon && <span>{subItem.icon}</span>} {/* Render icon if available */}
+                    <span>{subItem.title}</span>
                   </Link>
                 </li>
               ))}
