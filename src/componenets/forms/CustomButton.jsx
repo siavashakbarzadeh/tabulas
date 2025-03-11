94f68c5d02586dbd177620b6c8a26fac44af72d5
mralikeshtkar <alikeshtkar262@gmail.com>
2025-03-11 14:14:50 +0100

    sss

--- src/componenets/forms/CustomButton.jsx
+++ src/componenets/forms/CustomButton.jsx
@@ -5,17 +5,17 @@ function CustomButton({ label, onClick, isLoading = false, disabled = false }) {
   return (
     <>
       {isLoading ? (
+        <div className="w-full h-10 rounded-md bg-primary-900/20">
+          <Loading />
+        </div>
+      ) : (
         <button
           onClick={onClick}
           disabled={disabled}
-          className="w-full h-10 rounded-md text-sm bg-primary-900 text-white border-none outline-none ring-0 focus:border-none focus:outline-none focus:ring-0 disabled:bg-primary-900/30"
+          className="w-full h-10 rounded-md text-sm bg-primary-900 text-white border-none outline-none ring-0 focus:border-none focus:outline-none focus:ring-0 disabled:bg-primary-900/20"
         >
           {label}
         </button>
-      ) : (
-        <div className="w-full h-10 rounded-md bg-primary-900/30">
-          <Loading />
-        </div>
       )}
     </>
   );
