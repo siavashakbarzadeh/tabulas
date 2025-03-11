309af0746e87c165ff47d27da869ac4c7153ed09
mralikeshtkar <alikeshtkar262@gmail.com>
2025-03-11 14:06:05 +0100

    add container to button

--- src/componenets/forms/CustomButton.jsx
+++ src/componenets/forms/CustomButton.jsx
@@ -2,13 +2,15 @@ import React from "react";
 
 function CustomButton({ label, onClick, disabled = false }) {
   return (
-    <button
-      onClick={onClick}
-      disabled={disabled}
-      className="w-full h-10 disabled:bg-primary-900/50 rounded-md text-sm bg-primary-900 text-white border-none outline-none ring-0 focus:border-none focus:outline-none focus:ring-0"
-    >
-      {label}
-    </button>
+    <div className="w-full flex">
+      <button
+        onClick={onClick}
+        disabled={disabled}
+        className="w-full h-10 disabled:bg-primary-900/50 rounded-md text-sm bg-primary-900 text-white border-none outline-none ring-0 focus:border-none focus:outline-none focus:ring-0"
+      >
+        {label}
+      </button>
+    </div>
   );
 }
 
