ad7150c479e9d381137ec69f96ada63e230ea35b
mralikeshtkar <alikeshtkar262@gmail.com>
2025-03-11 14:04:10 +0100

    aaa

--- src/componenets/forms/CustomButton.jsx
+++ src/componenets/forms/CustomButton.jsx
@@ -5,7 +5,7 @@ function CustomButton({ label, onClick, disabled = false }) {
     <button
       onClick={onClick}
       disabled={disabled}
-      className="w-full h-10 disabled:bg-primary-900/10 rounded-md text-sm bg-primary-900 text-white border-none outline-none ring-0 focus:border-none focus:outline-none focus:ring-0"
+      className="w-full h-10 disabled:bg-primary-900/50 rounded-md text-sm bg-primary-900 text-white border-none outline-none ring-0 focus:border-none focus:outline-none focus:ring-0"
     >
       {label}
     </button>
