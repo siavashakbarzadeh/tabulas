a6ab4429d63ef42baccd3578877a8af315329d57
Amirezalla <allahverdiamirreza@gmail.com>
2025-03-17 10:04:07 +0100

    Update index.jsx

--- src/pages/ultimdossier/index.jsx
+++ src/pages/ultimdossier/index.jsx
@@ -77,43 +77,19 @@ function Ultimidossierage() {
     .map((item, index) => {
       const tempElement = document.createElement("div");
       tempElement.innerHTML = item;
-
       const rows = Array.from(tempElement.children).map((child, idx) => (
-        <td
-          key={idx}
-          className="py-3 px-4 text-left"
-          style={{ verticalAlign: "middle" }}
-        >
-          {/* Wrap content in a div to apply display: ruby-text */}
-          <div style={{ display: "ruby-text" }}>
-            {child.tagName === "A" ? (
-              <a
-                href={child.href}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i
-                  className="fas fa-file-pdf mr-2 custom-pdf-icon"
-                  style={{ color: "rgb(151, 0, 45)" }}
-                ></i>
-                {child.textContent}
-              </a>
-            ) : (
-              <span
-                dangerouslySetInnerHTML={{ __html: child.innerHTML }}
-              />
+        <td key={idx} className="py-3 px-4 text-left">
+          {child.tagName === "A" ?
+            <a href={child.href} target="_blank" rel="noopener noreferrer">
+              {child.textContent}
+            </a>
+            : (
+              <span dangerouslySetInnerHTML={{ __html: child.innerHTML }}></span>
             )}
-          </div>
         </td>
       ));
-
-      return (
-        <tr key={index} className="border-b">
-          {rows}
-        </tr>
-      );
+      return <tr key={index} className="border-b">{rows}</tr>;
     });
-
 
   return (
     <div className="flex flex-col min-h-screen w-full">
