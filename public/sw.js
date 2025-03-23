ffcb3d182d07b9a53875f8f528d8f4ac2bfee530
Amirezalla <allahverdiamirreza@gmail.com>
2025-03-23 19:55:56 +0100

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,19 +1,35 @@
-// public/sw.js
-self.addEventListener('push', event => {
-    const data = event.data ? event.data.json() : {};
-    const title = data.title || 'New Notification';
+// sw.js
+self.addEventListener("push", event => {
+    console.log("Push event received:", event);
+
+    let data = {};
+    if (event.data) {
+        try {
+            data = event.data.json();
+        } catch (err) {
+            console.error("Error parsing push data", err);
+        }
+    }
+
+    const title = data.title || "New Notification";
     const options = {
-        body: data.body || 'You have a new message!',
-        icon: data.icon || '/icon.png',
-        data: data.url || '/'
+        body: data.body || "You have a new message",
+        icon: data.icon || "",
+        data: {
+            url: data.url || "/"
+        }
     };
 
     event.waitUntil(self.registration.showNotification(title, options));
 });
 
-self.addEventListener('notificationclick', event => {
+self.addEventListener("notificationclick", event => {
     event.notification.close();
     event.waitUntil(
-        clients.openWindow(event.notification.data)
+        clients.matchAll({ type: "window" }).then(clientList => {
+            if (clients.openWindow) {
+                return clients.openWindow(event.notification.data.url);
+            }
+        })
     );
 });
