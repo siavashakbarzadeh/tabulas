1781097bd5f86d3daf8a9f80b480184cf1771006
Amirezalla <allahverdiamirreza@gmail.com>
2025-05-06 10:33:16 +0200

    Revert "Update vite.config.js"
    This reverts commit e87322d79d17b0303f232447200760679212318a.

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: { alias: { "react-native": "react-native-web" } },
-  build: {
-    rollupOptions: {
-      external: [
-        "expo", "expo-auth-session", "expo-file-system",
-        "expo-constants", "expo-crypto", "@expo/vector-icons"
-      ],
+  resolve: {
+    alias: {
+      "react-native": "react-native-web",
     },
   },
   server: {
