b83473d42a9073cf444a83d3ed0c3f13ddc9858b
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2025-05-21 16:16:49 +0330

    1

--- src/pages/login/CordovaAutoLogin.jsx
+++ src/pages/login/CordovaAutoLogin.jsx
@@ -1,30 +1,35 @@
 import { useEffect } from "react";
-import { useAuth } from "../../contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext.jsx";
 
-const isCordova = () => typeof window !== "undefined" && window.cordova;
-
-export default function CordovaAutoLogin() {
+const CordovaAutoLogin = () => {
   const { login } = useAuth();
   const navigate = useNavigate();
 
   useEffect(() => {
     const isCordova = typeof window !== "undefined" && window.cordova;
+
     if (!isCordova) return;
-  
-    const fakeToken = "dev-token";
-    const fakeUser = {
-      name: "Cordova Dev User",
-      email: "dev@mobile.app",
-      roles: ["admin"],
-      id: 9999,
-    };
-  
-    login(fakeToken, fakeUser);
-    navigate("/");
+
+    document.addEventListener(
+      "deviceready",
+      () => {
+        const fakeToken = "dev-token";
+        const fakeUser = {
+          name: "Cordova Dev User",
+          email: "dev@mobile.app",
+          roles: ["admin"],
+          id: 9999,
+        };
+
+        login(fakeToken, fakeUser);
+        navigate("/");
+      },
+      { once: true }
+    );
   }, []);
-  
 
   return null;
-}
+};
 
+export default CordovaAutoLogin;
