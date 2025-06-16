5637a20ae23f3d1169bf5790fbe35802f2619cfc
Amirezalla <allahverdiamirreza@gmail.com>
2025-06-16 07:56:07 +0200

    1

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: { alias: { "react-native": "react-native-web" } },
-  build: {
-    rollupOptions: {
-      external: [
-        "expo", "expo-auth-session", "expo-file-system",
-        "expo-constants", "expo-crypto", "@expo/vector-icons"
-      ],
-    },
-  },
   server: {
     port: 5201
   },
