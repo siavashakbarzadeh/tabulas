c600a703ba505958dce2edc5522c98c01591c3ee
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2025-06-16 07:13:45 +0200

    1

--- src/configs/msalConfig.js
+++ src/configs/msalConfig.js
@@ -7,7 +7,7 @@ const msalConfig = {
   auth: {
     clientId: "aa825561-377d-4414-8acc-2905cd587e98",
     authority: "https://login.microsoftonline.com/16c24428-0bd3-4bc1-a192-d315f43f5bb4",
-    redirectUri: isCordova ? "http://localhost" : "https://tabulas.vercel.app",
+    redirectUri: isCordova ? "tabulas://auth" : "https://tabulas.vercel.app",
   },
   cache: {
     cacheLocation: isCordova ? "localStorage" : "sessionStorage",
