fe1bf2c394c50eaf0c998cb8892d73ff8e645476
Amirezalla <allahverdiamirreza@gmail.com>
2025-06-16 08:15:56 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: { alias: { "react-native": "react-native-web" } },
+  // resolve: { alias: { "react-native": "react-native-web" } },
   build: {
     rollupOptions: {
       external: [
